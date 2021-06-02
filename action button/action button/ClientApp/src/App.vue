85ea62ef0afa55efed4df6db20c73687d6bfc494
Prompsy <bastien.prompsy@suez.com>
2021-06-02 10:31:55 +0200

    multiselect

---
+++ action button/action button/ClientApp/src/App.vue
@@ -0,0 +1,31 @@
+<template>
+    <meta charset="utf-8" />
+    <nav-menu></nav-menu>
+    <router-view />
+</template>
+
+<script>
+    import NavMenu from './components/NavMenu.vue'
+
+export default {
+        name: 'App',
+  components: {
+      NavMenu
+  }
+}
+</script>
+
+<style>
+    @font-face {
+        font-family: "DINpro";
+        src: url(./font/DINPro-Cond.woff) format("woof");
+    }
+#app {
+  font-family: DINpro ,Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ action button/action button/ClientApp/src/router/index.js
@@ -0,0 +1,30 @@
+﻿import { createWebHistory, createRouter } from "vue-router";
+import actionbutton from "@/components/actionbutton.vue";
+import summary from "@/components/summary.vue";
+import FetchData from "@/components/FetchData.vue";
+
+
+const routes = [
+    {
+        path: "/",
+        name: "actionbutton",
+        component: actionbutton,
+    },
+    {
+        path: "/summary",
+        name: "summary",
+        component: summary,
+    },
+    {
+        path: "/FetchData",
+        name: "FetchData",
+        component: FetchData,
+    }
+];
+
+const router = createRouter({
+    history: createWebHistory(),
+    routes,
+});
+
+export default router;
